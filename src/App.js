bbe830733fd3e36eac80f1390572f961cea87297
Kirill Kazakov <k@kirikaza.ru>
2016-12-17 21:49:04 +0400

    Clean BT channel before requesting, fix reading float temperature, remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
-/**
- * Created by sylor on 16.12.16.
- */
 import React, { Component } from 'react'
 import {
 	StyleSheet,
 	Text,
 	TouchableOpacity,
-	View,
-	Platform,
-	Switch
+	View
 } from 'react-native'
 
 import BluetoothSerial from 'react-native-bluetooth-serial'
-import { Buffer } from 'buffer'
-global.Buffer = Buffer
-const iconv = require('iconv-lite')
 
 const DEVICE_ADDR = '20:14:04:15:36:25';
+const ASK_TEMPERATURE_REQUEST = 't';
+const REREAD_ANSWER_DELAY = 100;
 
-const Button = ({ label, onPress }) =>
-	<TouchableOpacity style={styles.button} onPress={onPress}>
-		<Text style={{ color: '#fff' }}>{label}</Text>
-	</TouchableOpacity>
-
-class BluetoothSerialExample extends Component {
+class Bapometp extends Component {
 	constructor (props) {
 		super(props);
 		this.state = {
@@ -31,60 +20,14 @@ class BluetoothSerialExample extends Component {
 			connected: false,
 			connecting: false,
 			temperature: null,
+			asking: false,
 			debug: ''
 		}
 	}
 
 	componentWillMount () {
-		Promise.all([
-			BluetoothSerial.isEnabled()
-		])
-			.then((values) => {
-				const [ enabled ] = values;
-				this.setState({ btEnabled: enabled });
-			});
-
-		BluetoothSerial.on('bluetoothEnabled', () => {
-			this.setState({ btEnabled: true });
-		});
-		BluetoothSerial.on('bluetoothDisabled', () => {
-			this.setState({ btEnabled: false });
-		});
-		BluetoothSerial.on('connectionLost', () => {
-			this.setState({ connecting: false, connected: false });
-		});
-	}
-
-	connect () {
-		if (this.state.btEnabled && !this.state.connected && !this.state.connecting) {
-			this.setState({ connecting: true });
-			BluetoothSerial.connect(DEVICE_ADDR)
-				.then((res) => {
-					this.setState({ connected: true, connecting: false });
-				})
-				.catch((err) => {
-					this.setState({ connecting: false, debug: this.state.debug + '\nconnect caught err: ' + err });
-				});
-		}
-	}
-
-	write (message) {
-		if (this.state.connected) {
-			BluetoothSerial.write('t')
-				.then((res) => {
-					this.setState({ temperature: null });
-					BluetoothSerial.readUntil('\n')
-						.then((read) => {
-							this.setState({ temperature: read });
-						})
-						.catch((err) => {
-							this.setState({ debug: this.state.debug + "\nsubscribe caught err: " + err });
-						})
-				})
-				.catch((err) => {
-					this.setState({ debug: this.state.debug + '\nwrite caught err: ' + err });
-				})
-		}
+		this.subscribeBtEvents();
+		this.checkBtEnabled();
 	}
 
 	render () {
@@ -101,71 +44,116 @@ class BluetoothSerialExample extends Component {
 				</View>
 
 				<View style={{ backgroundColor: '#FC9' }}>
-					<Text>t = {this.state.temperature}ºC</Text>
+					<Text>{this.state.temperature === null ? 'no temperature' : this.state.temperature + 'ºC'}</Text>
 				</View>
 
-				<View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-					<Button
-						label='Write to device'
-						onPress={this.write.bind(this)}
-					/>
-					<Button
-						label='Connect'
-						onPress={this.connect.bind(this)}
-					/>
+				<View>
+					<TouchableOpacity onPress={() => this.askTemperature()}>
+						<Text>Is it hot?</Text>
+					</TouchableOpacity>
 				</View>
 			</View>
 		)
 	}
+
+	checkBtEnabled() {
+		Promise.all([
+			BluetoothSerial.isEnabled()
+		])
+			.then((values) => {
+				const [ enabled ] = values;
+				this.setBtEnabled(enabled);
+			});
+	}
+
+	subscribeBtEvents() {
+		BluetoothSerial.on('bluetoothEnabled', () => {
+			setBtEnabled(true);
+		});
+		BluetoothSerial.on('bluetoothDisabled', () => {
+			setBtEnabled(false);
+		});
+		BluetoothSerial.on('connectionLost', () => {
+			this.setState({ connecting: false, connected: false });
+		});
+	}
+
+	setBtEnabled(enabled) {
+		this.setState({ btEnabled: enabled, connecting: false, connected: false });
+		if (enabled) {
+			this.connect();
+		}
+	}
+
+	connect() {
+		if (!this.state.btEnabled) {
+			return;
+		}
+		if (this.state.connected || this.state.connecting) {
+			return;
+		}
+		this.setState({ connecting: true });
+		BluetoothSerial.connect(DEVICE_ADDR)
+			.then(() => {
+				this.setState({ connected: true, connecting: false, asking: false });
+			})
+			.catch((err) => {
+				this.setState({ connecting: false, debug: this.state.debug + '\nconnect caught err: ' + err });
+			});
+	}
+
+	askTemperature() {
+		if (!this.state.connected) {
+			return;
+		}
+		if (this.state.asking) {
+			return;
+		}
+		this.setState({ asking: true });
+		BluetoothSerial.clear()
+			.then(() => {
+				BluetoothSerial.write(ASK_TEMPERATURE_REQUEST)
+					.then(() => {
+						this.readTemperatureAnswer();
+					})
+					.catch((err) => {
+						this.setState({ asking: false, debug: this.state.debug + '\nwrite caught err: ' + err });
+					})
+			})
+			.catch((err) => {
+				this.setState({ asking: false, debug: this.state.debug + '\nclear caught err: ' + err });
+			});
+	}
+
+	readTemperatureAnswer() {
+		let taskId = setInterval(() => {
+			BluetoothSerial.readUntil('\n')
+				.then((read) => {
+					if (!read.length) {
+						return;
+					}
+					clearInterval(taskId);
+					this.setState({ asking: false, temperature: parseFloat(read)});
+				})
+				.catch((err) => {
+					clearInterval(taskId);
+					this.setState({ asking: false, debug: this.state.debug + "\nreadUntil caught err: " + err });
+				})
+		}, REREAD_ANSWER_DELAY);
+	}
+
 }
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		backgroundColor: '#F5FCFF'
+		flex: 1
 	},
 	heading: {
 		fontWeight: 'bold',
 		fontSize: 24,
 		marginVertical: 10,
 		alignSelf: 'center'
-	},
-	enableInfoWrapper: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		height: 40,
-		paddingHorizontal: 25,
-		alignItems: 'center'
-	},
-	connectionInfoWrapper: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		paddingHorizontal: 25
-	},
-	connectionInfo: {
-		fontWeight: 'bold',
-		alignSelf: 'center',
-		fontSize: 18,
-		marginVertical: 10,
-		color: '#238923'
-	},
-	listContainer: {
-		marginTop: 15,
-		borderColor: '#ccc',
-		borderTopWidth: 0.5
-	},
-	listItem: {
-		flex: 1,
-		padding: 25,
-		borderColor: '#ccc',
-		borderBottomWidth: 0.5
-	},
-	button: {
-		margin: 5,
-		padding: 25,
-		backgroundColor: '#4C4C4C'
 	}
-})
+});
 
-export default BluetoothSerialExample
+export default Bapometp
